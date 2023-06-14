4f0b592d45ab5c30e48a1a4c2efd9e81573b74bd
Ananya <ananyadharmaraj19@gmail.com>
2023-06-14 21:01:29 +0530

    Altered Homepage

--- app/src/main/java/com/example/miniproj/Splash.java
+++ app/src/main/java/com/example/miniproj/Splash.java
@@ -13,9 +13,9 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getSupportActionBar().hide();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getSupportActionBar().hide();
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
